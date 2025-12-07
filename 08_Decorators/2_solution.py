d39e4fa3c98bf4fbec48fed772a2879f80cf03ab
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-12-07 12:13:41 +0530

    debugging decorators

--- 08_Decorators/2_solution.py
+++ 08_Decorators/2_solution.py
@@ -1,1 +1,21 @@
 # debugging function call : create a decorator to print the function name and the value of its argument every timethe function is called.
+
+def debug(func):
+    def wrapper(*args,**kwargs):
+        arg_value = ", ".join(str(arg) for arg in args)
+        kwargs_value = ", ".join(f"{k}={v}" for k,v in kwargs.items())
+        print(f"calling : {func.__name__} with args {arg_value} and kwargs {kwargs_value}")
+        return func(*args,**kwargs)
+         
+    return wrapper
+
+@debug
+def hello():
+    print("hello")
+
+@debug
+def greet(name, greeting="helo !!!"):
+    print(f"{greeting}, {name}")
+
+hello()
+greet("chai", "hannji !!")
