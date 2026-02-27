3e5d694ba59af6af07b40e05a6f67ea40e86da32
sandeepsharma99 <sandeepsharma9182@gmail.com>
2026-02-27 20:31:17 +0530

    revision

--- 02_conditionals/1_question.py
+++ 02_conditionals/1_question.py
@@ -48,22 +48,4 @@ else:
 # vnxcmxzbncvnxbz
 # vnxcmxzbncvnxbz
 # vnxcmxzbncvnxbz
-# vnxcmxzbncvnxbz
-# vnxcmxzbncvnxbz
-# vnxcmxzbncvnxbz
-# vnxcmxzbncvnxbz
-# vnxcmxzbncvnxbz
-# vnxcmxzbncvnxbz
-# vnxcmxzbncvnxbz
-# vnxcmxzbncvnxbz
-# vnxcmxzbncvnxbz
-# vnxcmxzbncvnxbz
-# vnxcmxzbncvnxbz
-# vnxcmxzbncvnxbz
-# vnxcmxzbncvnxbz
-# vnxcmxzbncvnxbz
-# vnxcmxzbncvnxbz
-# vnxcmxzbncvnxbz
-# vnxcmxzbncvnxbz
-# vnxcmxzbncvnxbz
 
