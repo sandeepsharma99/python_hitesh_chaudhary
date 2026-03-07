0c0fa7e5faab311339d797ea5f310c0e8c467462
sandeepsharma99 <sandeepsharma9182@gmail.com>
2026-03-07 21:12:35 +0530

    bubble sort revision

--- DSA/12.BubbleSort.py
+++ DSA/12.BubbleSort.py
@@ -1,1 +1,11 @@
 def bubbleSort(a):
+    n = len(a)
+
+    for i in range(n):
+        for j in range(n-1-i):
+            if( a[j] >a[j+1]):
+                a[j],a[j+1] = a[j+1], a[j]
+a = [64,32,25,45,40,2]
+
+bubbleSort(a)
+print(a)
