298b6e485c42f8521cae5ea5d88730cada09e245
sandeepsharma99 <sandeepsharma9182@gmail.com>
2026-03-10 20:54:32 +0530

    divide and conquer concept

--- DSA/15.quickSort.py
+++ DSA/15.quickSort.py
@@ -1,17 +1,29 @@
+# chatgpt code  
 def QuickSort(arr,l,r):
     if(l<r):
         p = partition(arr,l,r)
-    
-    QuickSort(arr,l,p-1)
-    QuickSort(arr,p+1,r)
+        QuickSort(arr,l,p-1)
+        QuickSort(arr,p+1,r)
 
 def partition(arr,l,r):
     pivot = arr[l]
     i = l+1
-    j=r
+    j = r
 
     while True:
-        while(i<j and arr[i]<pivot):
-            i = i+1
-        while(i<j and arr[j]>pivot):
-            j = j-1
+        while(i<=j and arr[i] < pivot):
+            i = i + 1
+        while(i<=j and arr[j] > pivot):
+            j = j - 1
+
+        if(i < j):
+            arr[i], arr[j] = arr[j], arr[i]
+        else:
+            break
+
+    arr[l], arr[j] = arr[j], arr[l]
+    return j
+
+arr = [23,45,12,65,34,10,3]
+QuickSort(arr,0,len(arr)-1)
+print(arr)
